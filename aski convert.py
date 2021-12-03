704ab944584e3056c98d751163d158cb4a154803
Harrish <92968388+Harrishtamilarasan@users.noreply.github.com>
2021-12-03 19:01:11 +0530

    Add files via upload

---
+++ aski convert.py
@@ -0,0 +1,22 @@
+from tkinter import*
+root=Tk()
+root.geometry("500x500")
+root.configure(background = 'deep sky blue')
+
+enter_word = Entry(root)
+enter_word.place(relx=0.5,rely=0.4,anchor=CENTER)
+
+label = Label(root, text = "Name In Ascii : ", bg="gold" , fg="black")
+
+def asciiConverter():
+    input_word = enter_word.get()
+    
+    for letter in input_word :
+        label["text"] += str(ord(letter)) + " "
+        
+btn=Button(root,text="Show name in AScii",command=asciiConverter, bg="medium orchid", fg="black")
+btn.place(relx=0.5,rely=0.5,anchor=CENTER)
+
+label.place(relx=0.5,rely=0.6,anchor=CENTER)
+
+root.mainloop()
